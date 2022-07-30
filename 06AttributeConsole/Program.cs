99425c209e8c7b01b861b28a4c8588d717bc8067
张耀文 <1650860607@qq.com>
2022-07-30 12:25:50 +0800

    06特性

---
+++ 06AttributeConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using Library;
+using System.Reflection;
+
+namespace AttributeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 获取全部特性
+            Type type = typeof(MyClass);
+            object[]? attributes = type.GetCustomAttributes(true);//获取全部特性
+            if (type.IsDefined(typeof(MyTestAttribute), true))//判断是否存在指定类型的特性
+            {
+                //获取指定类型的全部特性
+                attributes = type.GetCustomAttributes(typeof(MyTestAttribute), true);
+            }
+            #endregion
+
+            #region 获取成员特性
+            PropertyInfo? propertyInfo = type.GetProperty("IntProperty");
+            attributes = propertyInfo?.GetCustomAttributes(true);//获取属性全部特性
+            if ((propertyInfo != null) && propertyInfo.IsDefined(typeof(MyTestAttribute), true))//判断是否存在指定类型的特性
+            {
+                //获取指定类型的全部特性
+                attributes = propertyInfo.GetCustomAttributes(typeof(MyTestAttribute), true);
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Library/MyAttribute.cs
@@ -0,0 +1,47 @@
+﻿namespace Library
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+    public class MyTestAttribute : Attribute
+    {
+        public string? Name { get; set; }
+
+        public MyTestAttribute()
+        {
+
+        }
+
+        public MyTestAttribute(int para)
+        {
+
+        }
+    }
+
+    [MyTest(Name = "MyClass")]
+    public class MyClassBase
+    {
+        private string? _strField;
+
+        [MyTest(0)]
+        public int IntProperty { get; set; }
+
+
+        public string? StrProperty
+        {
+            [return: MyTest]
+            get { return _strField; }
+            [param: MyTest]
+            set { _strField = value; }
+        }
+
+        [return: MyTest]
+        public string? MyClassFunction([MyTest] int para)
+        {
+            return null;
+        }
+    }
+
+    public class MyClass : MyClassBase
+    {
+
+    }
+}
